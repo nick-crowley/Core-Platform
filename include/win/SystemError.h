0516fa781e59aae4ec1a7590ed2e3596a429b641
nick-crowley <nicholas.crowley@gmail.com>
2023-01-17 21:59:35 +0000

    feat: Added `core::win::system_error` exception supporting formatted arguments

---
+++ include/win/SystemError.h
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef CorePlatform_h_included
+#	error Including this header directly may cause a circular dependency; include <corePlatform.h> directly
+#endif
+#include "nstd/FormatString.h"
+#include "nstd/SourceLocation.h"
+#include "../../src/PlatformSdk.h"
+
+namespace core::win
+{
+	struct system_error : public std::system_error
+	{
+		using base = std::system_error;
+		
+		explicit
+		system_error(::LRESULT code)
+			: base{static_cast<int>(code), std::system_category()}
+		{
+		}
+
+		template <typename... Params>
+		system_error(::LRESULT code, std::string_view msg, Params&&... args)
+		  : base{static_cast<int>(code), 
+		         std::system_category(), 
+		         std::vformat(msg,std::make_format_args(args...))}
+		{
+		}
+	};
+}
