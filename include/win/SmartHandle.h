8d2654a630d96f2d8dd4671af34a6bbd2c73c8bc
nick-crowley <nicholas.crowley@gmail.com>
2023-02-03 22:04:55 +0000

    feat: 'win::SmartHandle' should support weak-references and providing custom release delegates at runtime

--- include/win/SmartHandle.h
+++ include/win/SmartHandle.h
@@ -1,65 +1,125 @@
 #pragma once
 #include "library/core.Platform.h"
 
+namespace core::meta 
+{
+    template <typename T>
+    concept SmartHandleTraits = requires {
+        T::release(T::empty);
+    };
+}
+
 namespace core::win
 {
     namespace detail
     {   
-        auto constexpr
-        inline CloseHandlePolicy = [](::HANDLE handle) noexcept {
-            ::CloseHandle(handle);
-        };
-
-        auto constexpr
-        inline RegCloseKeyPolicy = [](::HKEY key) noexcept {
-            ::RegCloseKey(key);
-        };
-        
-        auto constexpr
-        inline FreeLibraryPolicy = [](::HMODULE handle) noexcept {
-            ::FreeLibrary(handle);
-        };
-        
-        using InvalidHandle = std::integral_constant<::HANDLE,INVALID_HANDLE_VALUE>;
-    } 
-
+        template <std::semiregular RawHandle>
+        struct HandleTraits;
+    }
+    
     //! @brief  Smart-handle with shared-ownership semantics
-    template <typename RawHandle, 
-              auto Releaser = detail::CloseHandlePolicy, 
-              auto Empty = nullptr>
+    template <std::semiregular  RawHandle, 
+              meta::SmartHandleTraits  Traits = detail::HandleTraits<RawHandle>>
     class SmartHandle
     {
-        using type = SmartHandle<RawHandle,Releaser,Empty>;
+        using type = SmartHandle<RawHandle,Traits>;
+        using reference = type&;
         
-        auto constexpr 
-        inline static Deleter = [](RawHandle* ptr) 
+        struct IHandleResource
         {
-            if (ptr && *ptr != Empty)
-            {
-                Releaser(*ptr);
+            satisfies(IHandleResource,
+                protected: IsDefaultConstructible,
+                public: IsPolymorphic noexcept
+            );
+
+            bool
+            virtual empty() const noexcept abstract;
+
+            RawHandle
+            virtual get() const noexcept abstract;
+            
+            void
+            virtual close() noexcept abstract;
+        };
+        
+        class BorrowedHandle : public IHandleResource 
+        {
+            RawHandle const  m_handle;
+
+        public:
+            BorrowedHandle(RawHandle h) 
+              : m_handle{h}
+            {}
+            
+        public:
+            bool
+            empty() const noexcept override {
+                return this->m_handle == Traits::empty;
+            }
+
+        public:
+            RawHandle
+            get() const noexcept override {
+                return this->m_handle;
+            }
+
+            void
+            close() noexcept override
+            {}
+        };
+        
+        template <std::invocable<RawHandle> ReleaseDelegate>
+        class ManagedHandle : public IHandleResource 
+        {
+            RawHandle const        m_handle;
+            ReleaseDelegate const  m_releaser;
+
+        public:
+            ManagedHandle(RawHandle h, ReleaseDelegate dx)
+              : m_handle{h}, m_releaser{std::move(dx)}
+            {}
+
+            ~ManagedHandle() noexcept {
+                this->close();
+            }
+            
+        public:
+            bool
+            empty() const noexcept override {
+                return this->m_handle == Traits::empty;
+            }
+
+        public:
+            RawHandle
+            get() const noexcept override {
+                return this->m_handle;
+            }
+
+            void
+            close() noexcept override {
+                this->m_releaser(this->m_handle);
             }
         };
-
+        
     private:
-        std::shared_ptr<RawHandle> m_handle;
+        std::shared_ptr<IHandleResource> m_object;
 
     public:
-        constexpr
+        template <std::invocable<RawHandle> ReleaseDelegate = decltype(Traits::release)>
         explicit
-        SmartHandle(RawHandle handle) 
-          : m_handle{new RawHandle{handle},Deleter}
+        SmartHandle(RawHandle handle, ReleaseDelegate release = Traits::release) 
+          : m_object{new ManagedHandle{handle,std::move(release)}}
         {
         }
         
-        SmartHandle& 
-        operator=(RawHandle handle) 
+        SmartHandle(RawHandle handle, meta::weakref_t) 
+          : m_object{new BorrowedHandle{handle}}
         {
-            type{handle}.swap(*this);
-            return *this;
         }
-
+        
         satisfies(SmartHandle,
-            constexpr IsRegular noexcept
+            IsRegular noexcept,
+            NotSortable
         );
         
     public:
@@ -67,69 +127,107 @@ namespace core::win
         void
         static close(type& handle) noexcept
         {
-            Releaser(*handle);
+            if (!handle->empty())
+                handle.m_object->close();
         }
         
         bool
-        empty() const noexcept
-        {
-            return !this->m_handle ||
-                   *this->m_handle == Empty;
+        empty() const noexcept {
+            return !this->m_object || this->m_object->empty();
         }
 
-        RawHandle*
-        get() const noexcept
-        {
-            return this->m_handle.get();
-        }
-        
-        void 
-        reset() 
-        {
-            this->m_handle.reset();
-        }
-
-        void 
-        reset(RawHandle handle) 
-        {
-            this->m_handle.reset(new RawHandle{handle},Deleter);
-        }
-
-        void 
-        swap(type& r) noexcept
-        {
-            this->m_handle.swap(r.m_handle);
+        RawHandle
+        get() const noexcept {
+            return this->m_object->get();
         }
         
         RawHandle 
-        operator*() const noexcept
-        {
-            return *this->m_handle.get();
+        operator*() const noexcept {
+            return this->m_object->get();
         }
 
         implicit operator 
-        bool() const noexcept
-        {
+        bool() const noexcept {
             return !this->empty();
+        }
+
+    public:
+        void 
+        reset() {
+            this->m_object.reset();
+        }
+        
+        template <std::invocable<RawHandle> ReleaseDelegate = decltype(Traits::release)>
+        void 
+        reset(RawHandle handle, ReleaseDelegate&& release = Traits::release) {
+            type{handle,release}.swap(*this);
+        }
+        
+        void 
+        reset(RawHandle handle, meta::weakref_t) {
+            type{handle}.swap(*this);
+        }
+
+        void 
+        swap(type& r) noexcept {
+            this->m_object.swap(r.m_handle);
         }
     };
     
-    //! @brief  Smart-handle released using ::CloseHandle()
+    namespace detail 
+    {
+        template <>
+        struct HandleTraits<::HANDLE> {
+            ::HANDLE constexpr
+            inline static empty = nullptr;
+
+            auto constexpr
+            inline static release = &::CloseHandle;
+        };
+        
+        template <>
+        struct HandleTraits<::HKEY> {
+            ::HKEY constexpr
+            inline static empty = nullptr;
+
+            auto constexpr
+            inline static release = &::RegCloseKey;
+        };
+        
+        template <>
+        struct HandleTraits<::HMODULE> {
+            ::HMODULE constexpr
+            inline static empty = nullptr;
+
+            auto constexpr
+            inline static release = &::FreeLibrary;
+        };
+        
+        struct FileHandleTraits {
+            ::HANDLE constexpr
+            inline static empty = INVALID_HANDLE_VALUE;
+
+            auto constexpr
+            inline static release = &::CloseHandle;
+        };
+    }
+
+    //! @brief  Shared @c ::HANDLE released using @c ::CloseHandle()
     using SharedHandle = SmartHandle<::HANDLE>;
 
-    //! @brief  Smart-handle released using ::CloseHandle()
+    //! @brief  Shared @c ::HANDLE released using @c ::CloseHandle()
     using SharedEvent = SharedHandle;
     
     //! @brief  Smart-handle released using ::CloseHandle()
-    using SharedFile = SmartHandle<::HANDLE, detail::CloseHandlePolicy, detail::InvalidHandle::value>;
+    using SharedFile = SmartHandle<::HANDLE, detail::FileHandleTraits>;
     
-    //! @brief  Smart-handle released using ::RegCloseKey()
-    using SharedRegistryKey = SmartHandle<::HKEY, detail::RegCloseKeyPolicy>;
+    //! @brief  Shared @c ::HKEY released using @c ::RegCloseKey()
+    using SharedRegistryKey = SmartHandle<::HKEY>;
 
-    //! @brief  Smart-handle released using ::FreeLibrary()
-    using SharedModule = SmartHandle<::HMODULE, detail::FreeLibraryPolicy>;
+    //! @brief  Shared @c ::HMODULE released using @c ::FreeLibrary()
+    using SharedModule = SmartHandle<::HMODULE>;
     
-    //! @brief  Smart-handle released using ::CloseHandle()
+    //! @brief  Shared @c ::HANDLE released using @c ::CloseHandle()
     using SharedThread = SharedHandle;
     
 } // namespace core::win
