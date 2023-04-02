c56f3cbcc1a3e7eec8ee0e4e882edc4e07abf292
nick-crowley <nicholas.crowley@gmail.com>
2023-04-02 14:00:23 +0100

    refactor: Replaced usage of non-standard `lambda()` after recent refactoring are causing compiler warnings

--- include/nstd/Functional.h
+++ include/nstd/Functional.h
@@ -6,17 +6,17 @@ namespace nstd
 {
 	template <typename Result>
 	auto constexpr 
-	inline cast_to = lambda(=, &arg, reinterpret_cast<Result>(arg));
+	inline cast_to = [](auto& arg) { return reinterpret_cast<Result>(arg); };
 	
 
 	template <typename Result>
 	auto constexpr 
-	inline change_cv = lambda(=, &arg, const_cast<Result>(arg));
+	inline change_cv = [](auto& arg) { return const_cast<Result>(arg); };
 
 
 	template <typename Result>
 	auto constexpr 
-	inline convert_to = lambda(=, &arg, static_cast<Result>(arg));
+	inline convert_to = [](auto& arg) { return static_cast<Result>(arg); };
 
 
 	template <typename Signature>
