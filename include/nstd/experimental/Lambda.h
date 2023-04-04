03f5648c16c35a5ec2ab4dd3bb04b013494083bc
nick-crowley <nicholas.crowley@gmail.com>
2023-04-04 21:45:45 +0100

    docs: Added copyright and layout to `<nstd/Lambda.h>`

--- include/nstd/experimental/Lambda.h
+++ include/nstd/experimental/Lambda.h
@@ -1,22 +1,51 @@
+/* o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o */ /*!
+* @copyright	Copyright (c) 2023, Nick Crowley. All rights reserved.
+* 
+* Redistribution and use in source and binary forms, with or without modification, are permitted
+* provided that the following conditions are met:
+* 
+* 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+*    and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+*    conditions and the following disclaimer in the documentation and/or other materials provided
+*    with the distribution.
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+* FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+* WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* 
+* The views and conclusions contained in the software and documentation are those of the author 
+* and should not be interpreted as representing official policies, either expressed or implied, of
+* the projects which contain it.
+*/
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=o Preprocessor Directives o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
 #pragma once
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o Header Files o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
 #include "../../../src/libBoost.h"
 
-namespace nstd::detail
-{
-	#define _make_lambda_param(seq,ignored,param)                                                     \
-		auto param
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o Name Imports o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
 
-	#define _make_lambda(mode,paramsAndExpr)                                                          \
-		[mode](BOOST_PP_LIST_ENUM(                                                                    \
-			BOOST_PP_LIST_TRANSFORM(_make_lambda_param,~,BOOST_PP_LIST_FIRST_N(                       \
-				BOOST_PP_DEC(BOOST_PP_LIST_SIZE(paramsAndExpr)),                                      \
-				paramsAndExpr                                                                         \
-			))                                                                                        \
-		)) { return BOOST_PP_LIST_FIRST(BOOST_PP_LIST_REVERSE(paramsAndExpr)); }
-}
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o Forward Declarations o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
 
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o Macro Definitions o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
 namespace nstd
 {
+	#define _make_lambda_param(seq,ignored,param)                                                 \
+		auto param
+
+	#define _make_lambda(mode,paramsAndExpr)                                                      \
+		[mode](BOOST_PP_LIST_ENUM(                                                                \
+			BOOST_PP_LIST_TRANSFORM(_make_lambda_param,~,BOOST_PP_LIST_FIRST_N(                   \
+				BOOST_PP_DEC(BOOST_PP_LIST_SIZE(paramsAndExpr)),                                  \
+				paramsAndExpr                                                                     \
+			))                                                                                    \
+		)) { return BOOST_PP_LIST_FIRST(BOOST_PP_LIST_REVERSE(paramsAndExpr)); }
+
 	//! @brief	Condensed lambda definition syntax (non-standard)
 	//! 
 	//! @param  mode		Capture mode
@@ -30,9 +59,18 @@ namespace nstd
 	//! @example	lambda(=, const x, x == 42)      [](auto const x)     { return x == 42;  }
 	//! @example	lambda(=, *p, delete p)          [](auto* p) -> void  { return delete p; }
 	//! @example	lambda(=, *x, &y, *x = y)        [](auto* x, auto& y) { return *x = y;   }
-	#define  lambda(mode, param1, ...)                                                                \
-		_make_lambda(                                                                                 \
-			mode,                                                                                     \
-			BOOST_PP_LIST_APPEND((param1, BOOST_PP_NIL), BOOST_PP_VARIADIC_TO_LIST(__VA_ARGS__))      \
+	#define  lambda(mode, param1, ...)                                                            \
+		_make_lambda(                                                                             \
+			mode,                                                                                 \
+			BOOST_PP_LIST_APPEND((param1, BOOST_PP_NIL), BOOST_PP_VARIADIC_TO_LIST(__VA_ARGS__))  \
 		)
 }
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o Constants & Enumerations o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-o Class Declarations o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-o Non-member Methods o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o Global Functions o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=-o End of File o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
