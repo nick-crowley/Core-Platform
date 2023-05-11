97aa5ab9f7d067b506f72c32999b5d04005d2ec0
nick-crowley <nicholas.crowley@gmail.com>
2023-05-11 23:13:27 +0100

    refactor: Renamed `PrivilegeFlag::None` enumerator to `Disabled` to better reflect what `None` means

--- include/security/PrivilegeFlag.h
+++ include/security/PrivilegeFlag.h
@@ -37,7 +37,7 @@
 namespace core::security
 {
 	enum class PrivilegeFlag : uint32_t {
-		None = 0,
+		Disabled = 0,                                       //!< Not enabled
 		Enabled = SE_PRIVILEGE_ENABLED,                     //!< Enabled
 		EnabledByDefault = SE_PRIVILEGE_ENABLED_BY_DEFAULT, //!< Enabled by default
 		Removed = SE_PRIVILEGE_REMOVED,                     //!< Used to remove a privilege
