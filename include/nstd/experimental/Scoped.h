fd7a8fd72f2cc3a660cdc5fc8e8123c4540a83c2
nick-crowley <nicholas.crowley@gmail.com>
2023-08-28 15:13:06 +0100

    feat: Added `scoped`, a non-standard self-documenting coding idiom for identifying intentionally scoped codeblocks

--- include/nstd/experimental/Scoped.h
+++ include/nstd/experimental/Scoped.h
@@ -25,79 +25,18 @@
 */
 // o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=o Preprocessor Directives o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
 #pragma once
-#ifndef CorePlatform_h_included
-#	define CorePlatform_h_included
-#endif
-
-#if (defined(_MSVC_LANG) && _MSVC_LANG <= 202002L)                                                \
- || (defined(__clang__) && __cplusplus <= 202002L)
-#	error Core-Platform requires C++23
-#endif
-
-#ifdef __clang__
-#	error Core-Platform doesn't yet support clang compiler
-#endif
 // o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o Header Files o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
-#include "../../src/PlatformSdk.h"
-#include "../../src/StdLibrary.h"
-#include "../../src/libBoost.h"
 
-#include "../../src/library/PlatformExport.h"
-
-#include "nstd/experimental/abstract.h"
-#include "nstd/experimental/implicit.h"
-#include "nstd/experimental/intern.h"
-#include "nstd/experimental/lambda.h"
-#include "nstd/experimental/metafunc.h"
-#include "nstd/experimental/nameof.h"
-#include "nstd/experimental/satisfies.h"
-#include "nstd/experimental/scoped.h"
-#include "nstd/experimental/lengthof.h"
-#include "nstd/experimental/finally.h"
-#include "nstd/TypeTraits.h"
-#include "nstd/Concepts.h"
-#include "nstd/Bitset.h"
-#include "nstd/FormatString.h"
-#include "nstd/Iterator.h"
-#include "nstd/IoManip.h"
-#include "nstd/Memory.h"
-#include "nstd/Sequence.h"
-#include "nstd/SizeOf.h"
-#include "nstd/SourceLocation.h"
-#include "nstd/Tuple.h"
-#include "nstd/Utility.h"
-
-#include "meta/TagTypes.h"
-#include "meta/Settings.h"
-
-#include "core/Exceptions.h"
-#include "core/ThrowIfEmpty.h"
-#include "core/ThrowIfNot.h"
-#include "core/ThrowIfNull.h"
-#include "core/ThrowIfZero.h"
-#include "core/ThrowInvalidArg.h"
-#include "core/Invariant.h"
-#include "core/PostCondition.h"
-
-#include "core/CharacterConversion.h"
-#include "core/BitwiseEnum.h"
-#ifndef __clang__
-#	include "core/EnumNames.h"
-#	include "core/ThrowIfUndefined.h"
-#endif
-#include "core/ToHexString.h"
-#include "core/ToString.h"
-
-#include "nt/NtStatus.h"
-#include "win/LResult.h"
-#include "win/HResult.h"
-#include "win/DWord.h"
 // o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o Name Imports o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
 
 // o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o Forward Declarations o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
 
 // o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o Macro Definitions o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
-
+namespace nstd
+{
+	//! @brief	Scoped object life-time (non-standard)
+#	define scoped       
+} 
 // o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o Constants & Enumerations o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
 
 // o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-o Class Declarations o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
