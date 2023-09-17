95fd4ea63132634939741119fc75024f3671c79f
nick-crowley <nicholas.crowley@gmail.com>
2023-09-17 16:46:39 +0100

    test: Added minimal system tests for `win::Process`

---
+++ test/Process_SystemTests.cpp
@@ -0,0 +1,105 @@
+/* o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o */ /*!
+* @copyright	Copyright (c) 2023, Nick Crowley. All rights reserved.
+* 
+* Redistribution and use in source and binary forms, with or without modification, are permitted
+* provided that the following conditions are met:
+* 
+* 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+*    and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+*    conditions and the following disclaimer in the documentation and/or other materials provided
+*    with the distribution.
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+* FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+* WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* 
+* The views and conclusions contained in the software and documentation are those of the author 
+* and should not be interpreted as representing official policies, either expressed or implied, of
+* the projects which contain it.
+*/
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=o Preprocessor Directives o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+#ifndef DISABLE_SHARED_LIBRARY_SYSTEM_TESTS
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o Header Files o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+#include <gtest/gtest.h>
+#include <win/Process.h>
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o Name Imports o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+using namespace core;
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o Forward Declarations o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o Macro Definitions o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o Constants & Enumerations o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-o Class Declarations o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-o Non-member Methods o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o Global Functions o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=-~o Test Code o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+TEST(Process_ST, IdsOfRunningProcessesCanBeEnumerated) 
+{
+	DWORD const ourPid = ::GetCurrentProcessId();
+	bool containsOurPid = false;
+	bool containsAnyPid = false;
+
+	//! @test	Verify PIDs can be enumerated
+	for (auto const& pid : win::Process::ExistingProcessIdCollection{}) {
+		containsOurPid |= (pid == ourPid);
+		containsAnyPid = true;
+	}
+	
+	//! @post	More than zero PIDs are returned
+	EXPECT_TRUE(containsAnyPid);
+
+	//! @post	Enumerated PIDs include that of the calling process
+	EXPECT_TRUE(containsOurPid);
+}
+
+TEST(Process_ST, RunningProcessesCanBeEnumerated) 
+{
+	using namespace nstd::literals;
+	bool containsThisProcess = false;
+	bool containsAnyProcess = false;
+
+	//! @test	Verify running processes can be enumerated
+	for (win::Process const& process : win::Process::ExistingProcesses) {
+		containsThisProcess |= (process.path().filename().string() == "exePlatformTests.exe"_isv);
+		containsAnyProcess = true;
+	}
+	
+	//! @post	More than zero processes are returned
+	EXPECT_TRUE(containsAnyProcess);
+
+	//! @post	Enumerated processes include the calling process
+	EXPECT_TRUE(containsThisProcess);
+}
+
+TEST(Process_ST, LoadedModulesCanBeEnumerated) 
+{
+	using namespace nstd::literals;
+	bool containsKernel32 = false;
+	bool containsAnyModules = false;
+
+	//! @test	Verify loaded modules can be enumerated
+	for (win::Module const& module : win::currentProcess.modules()) {
+		containsKernel32 |= (module.path().filename().string() == "kernel32.dll"_isv);
+		containsAnyModules = true;
+	}
+	
+	//! @post	More than zero modules are returned
+	EXPECT_TRUE(containsAnyModules);
+
+	//! @post	Enumerated modules include Kernel32.dll
+	EXPECT_TRUE(containsKernel32);
+}
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=-o End of File o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+#endif	// DISABLE_SHARED_LIBRARY_SYSTEM_TESTS
