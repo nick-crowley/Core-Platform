c7d3d14099c6f411541e88442b5810aec79a52ee
nick-crowley <nicholas.crowley@gmail.com>
2023-09-29 20:13:49 +0100

    feat: Extended `nstd::sequence_element` to support any sequence

--- include/nstd/sequence/sequence_element.h
+++ include/nstd/sequence/sequence_element.h
@@ -41,22 +41,72 @@
 // o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-o Class Declarations o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
 namespace nstd
 {
-	//! @brief	Query the element type of a @c std::integer_sequence<>
-	template <IntegerSequence Sequence>
-	metafunc sequence_element;
-
+	//! @brief	Query the element type of any homogenous sequence
+	template <typename Sequence>
+	metafunc sequence_element : std::type_identity<core::meta::undefined_t> {};
+	
+	// Specialization for @c std::integer_sequence
 	template <typename Value, Value... Values>
-	metafunc sequence_element<std::integer_sequence<Value,Values...>> {
-		using type = Value;
-	};
+	metafunc sequence_element<std::integer_sequence<Value,Values...>> : std::type_identity<Value> {};
+	
+	// Specialization for @c nstd::type_sequence
+	template <typename T, typename... R>
+		requires (std::same_as<T,R> && ...)
+	metafunc sequence_element<type_sequence<T,R...>> : std::type_identity<T> {};
+	
+	// Specialization for @c nstd::value_sequence
+	template <typename Value, Value... Values>
+	metafunc sequence_element<value_sequence<Value,Values...>> : std::type_identity<Value> {};
+	
+	// Specialization for @c nstd::value_tuple
+	template <auto V, auto... R>
+		requires (std::same_as<decltype(V),decltype(R)> && ...)
+	metafunc sequence_element<value_tuple<V,R...>> : std::type_identity<decltype(V)> {};
 
-	template <IntegerSequence Sequence>
+
+	//! @brief  Element type of any homogenous sequence
+	template <typename Sequence>
 	using sequence_element_t = typename sequence_element<Sequence>::type;
 }
 // o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-o Non-member Methods o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
 
 // o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o Global Functions o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
 
-// o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o Separator o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=-~o Test Code o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+namespace nstd::testing {
+	//! @test  Verify @c nstd::sequence_element_t is @c undefined for non-sequence types
+	static_assert(std::same_as<sequence_element_t<int>, core::meta::undefined_t>);
+	
+	//! @test  Verify @c nstd::sequence_element_t returns element type of empty @c std::integer_sequence
+	static_assert(std::same_as<sequence_element_t<std::integer_sequence<int>>, int>);
 
+	//! @test  Verify @c nstd::sequence_element_t returns element type of empty @c nstd::value_sequence
+	static_assert(std::same_as<sequence_element_t<value_sequence<char>>, char>);
+	
+	//! @test  Verify @c nstd::sequence_element_t is @c undefined for empty @c nstd::value_tuple
+	static_assert(std::same_as<sequence_element_t<value_tuple<>>, core::meta::undefined_t>);
+
+	//! @test  Verify @c nstd::sequence_element_t is @c undefined for empty @c nstd::type_sequence
+	static_assert(std::same_as<sequence_element_t<type_sequence<>>, core::meta::undefined_t>);
+	
+	
+	//! @test  Verify @c nstd::sequence_element_t returns element type of non-empty @c std::integer_sequence
+	static_assert(std::same_as<sequence_element_t<std::integer_sequence<long,2,3,4>>, long>);
+
+	//! @test  Verify @c nstd::sequence_element_t returns element type of non-empty @c nstd::value_sequence
+	static_assert(std::same_as<sequence_element_t<value_sequence<bool,true,false>>, bool>);
+	
+	//! @test  Verify @c nstd::sequence_element_t returns element type of homogenous non-empty @c nstd::value_tuple
+	static_assert(std::same_as<sequence_element_t<value_tuple<1,2,3>>, int>);
+
+	//! @test  Verify @c nstd::sequence_element_t returns element type of homogenous non-empty @c nstd::type_sequence
+	static_assert(std::same_as<sequence_element_t<type_sequence<bool,bool,bool>>, bool>);
+	
+
+	//! @test  Verify @c nstd::sequence_element_t is @c undefined for heterogenous non-empty @c nstd::value_tuple
+	static_assert(std::same_as<sequence_element_t<value_tuple<1,true,2.0f>>, core::meta::undefined_t>);
+
+	//! @test  Verify @c nstd::sequence_element_t is @c undefined for heterogenous non-empty @c nstd::type_sequence
+	static_assert(std::same_as<sequence_element_t<type_sequence<bool,char,bool>>, core::meta::undefined_t>);
+}
 // o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=-o End of File o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
