c5a385a5a96f9c6801248eaddb6b7bfafd6c793a
nick-crowley <nicholas.crowley@gmail.com>
2023-10-07 19:32:27 +0100

    feat: Added `win::FileRight` to represent file-hand access rights

---
+++ include/security/FileRight.h
@@ -0,0 +1,82 @@
+/* o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o */ /*!
+* @copyright	Copyright (c) 2018, Nick Crowley. All rights reserved.
+* 
+* Redistribution and use in source and binary forms, with or without modification, are permitted
+* provided that the following conditions are met:
+* 
+* 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+*    and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+*    conditions and the following disclaimer in the documentation and/or other materials provided
+*    with the distribution.
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+* FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+* WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* 
+* The views and conclusions contained in the software and documentation are those of the author 
+* and should not be interpreted as representing official policies, either expressed or implied, of
+* the projects which contain it.
+*/
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=o Preprocessor Directives o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+#pragma once
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o Header Files o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+#include "library/core.Platform.h" 
+#include "security/AccessRight.h"
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o Name Imports o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o Forward Declarations o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o Macro Definitions o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o Constants & Enumerations o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+namespace core::win 
+{
+	/* ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` */ /*!
+	* @brief  Defines access rights for file, directories, and pipe objects
+	*/
+	enum class FileRight : access_mask_t { 
+		All = FILE_ALL_ACCESS,                      //!< All possible access rights for a file.
+		AddFile = FILE_ADD_FILE,                    //!< For a directory, the right to create a file in the directory.
+		AddFolder = FILE_ADD_SUBDIRECTORY,          //!< For a directory, the right to create a subdirectory.
+		AppendData = AddFolder,                     //!< For a file object, the right to append data to the file.
+		CreatePipeInstance = AddFolder,             //!< For a named pipe, the right to create a pipe.
+		DeleteChild = FILE_DELETE_CHILD,            //!< For a directory, the right to delete a directory and all the files it contains, including read-only files.
+		Execute = FILE_EXECUTE,                     //!< For a native code file, the right to execute the file.
+		ListDirectory = FILE_LIST_DIRECTORY,        //!< For a directory, the right to list the contents of the directory.
+		ReadAttributes = FILE_READ_ATTRIBUTES,      //!< The right to read file attributes.
+		ReadData = ListDirectory,                   //!< For a file object, the right to read the corresponding file data.
+		ReadExtended = FILE_READ_EA,                //!< The right to read extended file attributes.
+		Traverse = Execute,                         //!< For a directory, the right to traverse the directory.
+		WriteAttributes = FILE_WRITE_ATTRIBUTES,    //!< The right to write file attributes.
+		WriteData = AddFile,                        //!< For a file object, the right to write data to the file.
+		WriteExtended = FILE_WRITE_EA,              //!< The right to write extended file attributes.
+	};
+	
+	enum class CommonRight : access_mask_t;
+	enum class GenericRight : access_mask_t;
+	enum class StandardRight : access_mask_t;
+}
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o Constants & Enumerations o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+namespace core::meta 
+{
+	//! @brief	@c core::win::FileRight is a bitflag
+	metadata bool Settings<bitwise_enum, win::FileRight> = true;
+	
+	//! @brief	@c core::win::FileRight can be combined with common/standard/generic rights
+	metadata bool Settings<compatible_enum, win::FileRight, win::CommonRight> = true;
+	metadata bool Settings<compatible_enum, win::FileRight, win::GenericRight> = true;
+	metadata bool Settings<compatible_enum, win::FileRight, win::StandardRight> = true;
+}
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-o Class Declarations o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-o Non-member Methods o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o Global Functions o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=-o End of File o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
