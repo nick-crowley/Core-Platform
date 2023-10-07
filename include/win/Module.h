c08fc6bc98f64c8a1e73e1030e67538b7e3c6451
nick-crowley <nicholas.crowley@gmail.com>
2023-10-07 19:47:02 +0100

    fix: Corrected typo in copyright in `<win\Module.h>` and `<win\ResourceId.h>`

--- include/win/Module.h
+++ include/win/Module.h
@@ -1,7 +1,7 @@
 /* o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o */ /*!
 * @copyright	Copyright (c) 2023, Nick Crowley. All rights reserved.
 * 
-* Redistribution and use in source and binary win, with or without modification, are permitted
+* Redistribution and use in source and binary forms, with or without modification, are permitted
 * provided that the following conditions are met:
 * 
 * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
