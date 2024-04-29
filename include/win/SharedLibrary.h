59fcfdd445cd4cbbf1dc0374767f19c6e04b6179
nick-crowley <nicholas.crowley@gmail.com>
2024-04-29 21:53:05 +0100

    feat: `win::SharedLibrary` should support flags to adjust loading behaviour

--- include/win/SharedLibrary.h
+++ include/win/SharedLibrary.h
@@ -36,7 +36,28 @@
 // o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o Macro Definitions o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
 
 // o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o Constants & Enumerations o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+namespace core::win
+{
+	enum class LoadLibraryFlags : DWORD {
+		None                  = 0,
+		DontResolveReferences = DONT_RESOLVE_DLL_REFERENCES,         //!< System does not call DllMain for process and thread initialization and termination or load additional executable modules.
+		IgnoreCodeAuthzLevel  = LOAD_IGNORE_CODE_AUTHZ_LEVEL,        //!< System does not check AppLocker rules or apply Software Restriction Policies.
+		AsDatafile            = LOAD_LIBRARY_AS_DATAFILE,            //!< System maps the file into the calling process's virtual address space as if it were a data file.
+		AsDatafileExclusive   = LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE,  //!< Similar to LOAD_LIBRARY_AS_DATAFILE, except that the DLL file is opened with exclusive write access for the calling process.
+		AsImageResource       = LOAD_LIBRARY_AS_IMAGE_RESOURCE,      //!< System maps the file into the process's virtual address space as an image file.
+		SearchApplicationDir  = LOAD_LIBRARY_SEARCH_APPLICATION_DIR, //!< Application's installation directory is searched for the DLL and its dependencies.
+		SearchDefaultDirs     = LOAD_LIBRARY_SEARCH_DEFAULT_DIRS,    //!< This value is a combination of LOAD_LIBRARY_SEARCH_APPLICATION_DIR, LOAD_LIBRARY_SEARCH_SYSTEM32, and LOAD_LIBRARY_SEARCH_USER_DIRS.
+		SearchDllLoadDir      = LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR,    //!< Directory that contains the DLL is temporarily added to the beginning of the list of directories that are searched for dependencies.
+		SearchSystem32        = LOAD_LIBRARY_SEARCH_SYSTEM32,        //!< %windows%\system32 is searched for the DLL and its dependencies.
+		SearchUserDirs        = LOAD_LIBRARY_SEARCH_USER_DIRS,       //!< Directories added using the AddDllDirectory or the SetDllDirectory function are searched for the DLL and its dependencies.
+		AlteredSearchPath     = LOAD_WITH_ALTERED_SEARCH_PATH,       //!< System uses the alternate file search strategy discussed in the Remarks section
+		RequireSignedTarget   = LOAD_LIBRARY_REQUIRE_SIGNED_TARGET,  //!< Specifies that the digital signature of the binary image must be checked at load time.
+		SafeCurrentDirs       = LOAD_LIBRARY_SAFE_CURRENT_DIRS,      //!< Loading a DLL for execution from the current directory is only allowed if it is under a directory in the Safe load list.
+	};
+}
 
+//! @brief	@c core::win::LoadLibraryFlags is a bitflag
+metadata bool core::meta::Settings<core::bitwise_enum, core::win::LoadLibraryFlags> = true;
 // o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-o Class Declarations o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
 namespace core::win
 {
@@ -46,7 +67,8 @@ namespace core::win
 	class SharedLibrary
 	{
 		// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-o Types & Constants o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o
-
+	public:
+		using enum LoadLibraryFlags;
 		// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=o Representation o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o
 	private:
 		SharedModule  Handle;
@@ -54,11 +76,11 @@ namespace core::win
 	public:
 		SharedLibrary(std::string_view) = delete;
 
-		SharedLibrary(std::wstring_view path) 
-		  : Handle{::LoadLibraryW(ThrowIfEmpty(path).data())}
+		SharedLibrary(std::wstring_view path, LoadLibraryFlags flags = LoadLibraryFlags::None) 
+		  : Handle{::LoadLibraryExW(ThrowIfEmpty(path).data(), nullptr, std::to_underlying(flags))}
 		{
 			if (!this->Handle)
-				LastError{}.throwAlways("LoadLibrary() failed");
+				LastError{}.throwAlways("LoadLibraryExW() failed");
 		}
 		// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o Copy & Move Semantics o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o
 	public:
